f8b57f34ca5b4a94a63bd8f61a352593ec36659e
pablodiegoss <pablodiegoss@hotmail.com>
2018-12-05 12:58:56 -0200

    Add comments

--- src/questao3/stubs/Pessoa.java
+++ src/questao3/stubs/Pessoa.java
@@ -7,52 +7,92 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+/* Esta é a nossa classe Pessoa que irá se comunicar com outras pessoas
+ * É identificada por um nome único para questões semânticas da aplicação.
+ * Esta classe pode ser entendida como um componente da aplicação que irá se conectar 
+ * a outros componentes através do Registry e procurar "recursos" e enviar mensagens para outras pessoas.
+ */
 public class Pessoa implements Postman {
 
+	// Identificador único.
 	private String nome;
+
+	// Registro na qual esta pessoa pertence.
 	private Registry registro;
 
 	public Pessoa(String nome) throws RemoteException, MalformedURLException {
+		// A criação de uma pessoa e o preenchimento de seus atributos.
 		this.setNome(nome);
 		System.out.println(nome + ": Criado!");
 
+		// Neste momento iremos obter um objeto stub a partir desta pessoa.
 		Postman stub = (Postman) UnicastRemoteObject.exportObject(this, 0);
+
+		// Aqui obtemos o registro padrão através do get sem parametros, um registro na
+		// porta default 1099.
 		registro = LocateRegistry.getRegistry();
+
+		// Inserimos o stub desta pessoa no registro.
+		// este stub servirá como proxy para que objetos remotos acessem esta pessoa.
 		registro.rebind(getNome(), stub);
 
+		// A partir de agora este "componente" pessoa está conectado ao sistema e
+		// disponível para conexões.
 		System.out.println(nome + ": E registrado!");
 	}
 
+	/*
+	 * Método de enviar mensagens para outras pessoas. Recebendo como parâmetros a
+	 * mensagem e o destinatário. Esta decisão é apenas para facilitar a simulação
+	 * do sistema distribuido na Main.
+	 */
 	public void sendMessage(String message, String reciever) {
-			System.out.println(nome + ": Vou enviar uma mensagem para " + reciever);
-			System.out.println(nome + ": " + message);
+		System.out.println(nome + ": Vou enviar uma mensagem para " + reciever);
+		System.out.println(nome + ": " + message);
+
+		// Utilizando o registro desta pessoa iremos buscar um stub registrado com este
+		// nome salvo em reciever.
 		try {
-			
-			Postman pessoa;
-			pessoa = (Postman) registro.lookup(reciever);
+			// Caso a pessoa que procuro esteja registrada, um objeto proxy de Postman
+			// será retornado, sendo este o stub que foi registrado no Registry
+			// no construtor da classe pessoa.
+			Postman pessoa = (Postman) registro.lookup(reciever);
+
+			// Método Remoto definido na interface que recebe mensagens e a exibe na tema.
 			pessoa.receiveMessage(message, nome);
-			
 		} catch (RemoteException | NotBoundException e1) {
+			// Caso a execução remota não funcione temos a RemoteException
+			// Caso o destinatário não tenha realizado o Bind no Registry,
+			// este não está conectado e temos a NotBountException
 			e1.printStackTrace();
 		}
-
 	}
 
+	// Método para recebimento e exibição de mensagens.
 	public void receiveMessage(String message, String sender) {
 		System.out.println(nome + ": Recebi uma mensagem do " + sender + "!");
 		System.out.println(nome + ": " + sender + " diz: " + message);
 	}
 
+	// Método para envio de mensagens a todas as pessoas conectadas.
+	// O parâmetro message foi definido para melhor semântica na Main do sistema e
+	// simulação da execução distribuida.
 	public void sendBroadCast(String message) {
 		System.out.println(nome + ": Vou mandar mensagem pra todo mundo");
 		System.out.println(nome + ": " + message);
 
 		String[] pessoas;
 		try {
+			// Para enviar uma mensagem a todas as pessoas, podemos obter os nomes que
+			// realizaram Bind no registro.
 			pessoas = registro.list();
-			for (String pnome : pessoas) {
-				if (!pnome.equals(nome)) {
-					Postman p = (Postman) registro.lookup(pnome);
+			for (String nome : pessoas) {
+				// Como a pessoa atual também está conectada e disponível no registro,
+				// enviaremos mensagem a todas elas, menos a si mesmo.
+				if (!nome.equals(this.nome)) {
+					// Acesso ao stub de cada pessoa encontrada para que acessemos seu método
+					// remotamente.
+					Postman p = (Postman) registro.lookup(nome);
 					p.receiveMessage(message, nome);
 				}
 			}
@@ -61,6 +101,7 @@ public class Pessoa implements Postman {
 		}
 	}
 
+	// Getters and Setters.
 	public String getNome() {
 		return nome;
 	}
--- src/questao3/stubs/Servidor.java
+++ src/questao3/stubs/Servidor.java
@@ -1,19 +1,26 @@
 package questao3.stubs;
 
 import java.net.MalformedURLException;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.server.UnicastRemoteObject;
 
 public class Servidor {
+	/**Esta é uma classe servidor, para um software RMI 
+	deve ser criado um registro em alguma porta que os objetos remotos irão se conectar
+	Utilizando LocateRegistry.createRegistry(PORTA);
+	
+	Neste caso que é executado de forma não distríbuida, será utilizado o registro RMI padrão local da porta 1099.
+	*/
 	public static void main(String[] args) {
+		// Os objetos Pessoa aqui definidos representam instâncias que podem ser remotas da aplicação.
 		Pessoa p1 = null;
 		Pessoa p2 = null;
 		Pessoa p3 = null;
 		Pessoa p4 = null;
 		
 		try {
+			// A criação destes objetos pode retornar exceções relacionadas a conexão e a URL dos objetos.
+			// Devido a conexão realizada ao registro dentro do construtor
+			// Assim que você cria uma nova pessoa, esta irá se registrar ao sistema e estará disponível.
 			p1 = new Pessoa("Pablo");
 			p2 = new Pessoa("Rodrigo");
 			p3 = new Pessoa("Hugo");
@@ -21,8 +28,16 @@ public class Servidor {
 		} catch (RemoteException | MalformedURLException e) {
 			e.printStackTrace();
 		}
+		
+		// Abaixo temos alguns exemplos de módulos que poderiam ser executados remotamente através de um software distribuido.
 
-		p1.sendMessage("OI!", "Rodrigo");
+		// Um objeto remoto que irá tentar se comunicar com a pessoa de nome "Rodrigo"
+		p1.sendMessage("OI!", p2.getNome());
+		
+		// O Rodrigo irá enviar esta mensagem a todas as pessoas conectadas no sistema.
 		p2.sendBroadCast("OI GALERA!");
+		
+		// Outra pessoa se comunicando diretamente com nosso quarto exemplo.
+		p3.sendMessage("Olá", p4.getNome());
 	}
 }
